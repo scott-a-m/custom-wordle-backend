970be5acf473326bfa8ba47c2e72fa408a152dcd
Scott Mitchell <scott.a.mitchell1@hotmail.com>
2022-09-22 11:27:57 +0800

    some changes

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 require("dotenv").config();
 require("express-async-errors");
 
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
 const mongoose = require("mongoose");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSantize = require("express-mongo-sanitize");
+
 const {
   getWordle,
   createWordle,
 } = require("./controllers/worldeItemController");
 
+const express = require("express");
+const app = express();
+
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
+app.use(express.json());
 app.use(cors());
+app.use(xss());
+app.use(helmet());
+app.use(mongoSantize());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
 
 app.get("/:id", getWordle);
+app.get("/", (req, res) => {
+  res.send("Custom Wordle Back-end");
+});
 app.post("/", createWordle);
 
 const start = async () => {
